c530f8a0b6fe4c6ca2a0ab8348152e69a24e57db
ana.sdp <septianadwip219@gmail.com>
2022-08-21 00:19:56 +0700

    Prontera 1
    Tugas

--- funios-prontera-group-7-week1-quiz.playground/Contents.swift
+++ funios-prontera-group-7-week1-quiz.playground/Contents.swift
@@ -1,17 +1,17 @@
 import UIKit
 
 // MARK: Quiz 1
-/*  Fix the error */
+//  Fix the error
 
-let nama = "Samtoso"
-let umur = "20"
-let tinggiBadan = "180cm"
+var nama = "Samtoso"
+let umur = 20
+let tinggiBadan: Double = 180
 
-nama = ""
+nama = "Burhan"
 
 let umurDuaTahunLalu = umur - 2
 let tinggiBadanDibagiTiga = tinggiBadan / 3.2
-
+print("Hello nama saya \(nama), dua tahun lalu saya berumur \(umurDuaTahunLalu), tinggi saya jika dibagi \(tinggiBadanDibagiTiga) cm ")
 /* SUCCESS Parameter: bisa Print output `Halo, nama saya Burhan, dua tahun lalu saya berumur 18 tahun, tinggi saya jika dibagi 3 adalah 56.25 cm` */
 
 // MARK: Quiz 2
@@ -23,41 +23,45 @@ let tinggiBadanDibagiTiga = tinggiBadan / 3.2
  Jika nilai 70-79 mendapatkan nilai C.
  Jika nilai dibawah 69 mendapatkan nilai D.
  */
-let nilai = 90
-
-if nilai < 70 {
-    print("Kamu mendapatkan nilai D")
-} else if nilai > 70 {
-    print("Kamu mendapatkan nilai C")
-} else {
-    print("Kamu mendapatkan nilai B")
-} else if nilai > 90  {
+let nilai =  69
+//
+if nilai >= 90 && nilai <= 100 {
     print("Kamu mendapatkan nilai A")
+} else if nilai >= 80 && nilai <= 89 {
+    print("Kamu mendapatkan nilai B")
+} else if nilai >= 70 && nilai <= 79 {
+    print("Kamu mendapatkan nilai C")
+} else if nilai <= 69 {
+    print("Kamu mendapatkan nilai D")
 }
 
 switch nilai {
-case 70:
-    print("Kamu mendapatkan nilai D")
-case 90:
-    print("Kamu mendapatkan nilai D")
-case 60:
-    print("Kamu mendapatkan nilai D")
+case 90...100:
+    print("Kamu mendapatkan nilai A")
+case 80...89:
+    print("Kamu mendapatkan nilai B")
+case 70...79:
+    print("Kamu mendapatkan nilai C")
+case 0...69:
+    print("kamu mendapat nilai D")
+default:
+    print("")
 }
 
-/* SUCCESS Parameter: Jika {nilai} diganti dengan angka tertentu akan mengeluarkan output yang sesuai dengan kondisi */
-
+// SUCCESS Parameter: Jika {nilai} diganti dengan angka tertentu akan mengeluarkan output yang sesuai dengan kondisi
+//
 // MARK: Quiz 3
 /*
  Manipulasi array berikut agar dapat mengeluarkan output sesuai yang ditentukan
  */
-let kumpulanAngka: [Int]!
+var kumpulanAngka: [Int]!
 
 kumpulanAngka = [235, 212, 23]
-kumpulanAngka.append(contentsOf: 10)
-
-print("Saya memiliki kumpulan yang berisi ... angka acak")
-for ... in kumpulanAngka {
-    print("Diurutan ke-?? valuenya ??")
+kumpulanAngka.append(contentsOf: [10, 35])
+print("Saya memiliki kumpulan yang berisi \(kumpulanAngka.count) angka acak")
+//print(kumpulanAngka.enumerated())
+for (index,angka) in kumpulanAngka.enumerated() {
+    print("Diurutan ke \(index) valuenya \(angka)")
 }
 
 /* SUCCESS Parameter:
@@ -77,15 +81,23 @@ for ... in kumpulanAngka {
  Jika bisa dibagi 3 & 5: Print = FuniOS
  */
 
-func funiOS(total: Int?) {
-    for .... in .... {
-        if .. {
-            print("Fun")
+func funiOS(total: Int = 100) {
+    for a in 1...100 {
+        if a % 15 == 0 {
+            print("FuniOS")
+        }
+        else if a % 5 == 0 {
+            print("iOS")
+        }
+       else  if a % 3 == 0 {
+        print("Fun")
+        }
+        else {
+         print(a)
         }
     }
 }
-
-print(funIos(total: 100))
+funiOS(total: 100)
 
 /* SUCCESS Parameter: Print 100 angka/text sesuai dengan kondisi yang ditentukan
  Print output example:
